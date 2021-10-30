011ea39cc80bf92138e24dd1d7cc29123fbde173
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-31 01:11:32 +0800

    filled triangle

--- src/main.cpp
+++ src/main.cpp
@@ -38,9 +38,33 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
     swap(t0, t2);
   if (t1.y > t2.y)
     swap(t1, t2);
-  line(t0.x, t0.y, t1.x, t1.y, image, green);
-  line(t1.x, t1.y, t2.x, t2.y, image, green);
-  line(t0.x, t0.y, t2.x, t2.y, image, red);
+  int total_height = t2.y - t0.y;
+  for (int y = t0.y; y <= t1.y; y++) {
+    int segment_height = t1.y - t0.y + 1;
+    float alpha = (float)(y - t0.y) / total_height;
+    float beta =
+        (float)(y - t0.y) / segment_height; // be careful with divisions by zero
+    Vec2i A = t0 + (t2 - t0) * alpha;
+    Vec2i B = t0 + (t1 - t0) * beta;
+    if (A.x > B.x)
+      swap(A, B);
+    for (int j = A.x; j <= B.x; j++) {
+      image.set(j, y, color);
+    }
+  }
+  for (int y = t1.y; y <= t2.y; y++) {
+    int segment_height = t2.y - t1.y + 1;
+    float alpha = (float)(y - t0.y) / total_height;
+    float beta =
+        (float)(y - t1.y) / segment_height; // be careful with divisions by zero
+    Vec2i A = t0 + (t2 - t0) * alpha;
+    Vec2i B = t1 + (t2 - t1) * beta;
+    if (A.x > B.x)
+      swap(A, B);
+    for (int j = A.x; j <= B.x; j++) {
+      image.set(j, y, color);
+    }
+  }
 }
 
 int main(int argc, char **argv) {

1cff069a5a5222a6cc93db7336b35cfd560c0436
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-31 01:04:11 +0800

    triangle is fun

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-Model *model = NULL;
-const int width = 800;
-const int height = 800;
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const int width = 200;
+const int height = 200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   bool steep = false;
@@ -31,28 +31,29 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   }
 }
 
-int main(int argc, char **argv) {
-  if (2 == argc) {
-    model = new Model(argv[1]);
-  } else {
-    model = new Model("obj/african_head.obj");
-  }
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+  if (t0.y > t1.y)
+    swap(t0, t1);
+  if (t0.y > t2.y)
+    swap(t0, t2);
+  if (t1.y > t2.y)
+    swap(t1, t2);
+  line(t0.x, t0.y, t1.x, t1.y, image, green);
+  line(t1.x, t1.y, t2.x, t2.y, image, green);
+  line(t0.x, t0.y, t2.x, t2.y, image, red);
+}
 
+int main(int argc, char **argv) {
   TGAImage image(width, height, TGAImage::RGB);
-  for (int i = 0; i < model->nfaces(); i++) {
-    vector<int> face = model->face(i);
-    for (int j = 0; j < 3; j++) {
-      Vec3f v0 = model->vert(face[j]);
-      Vec3f v1 = model->vert(face[(j + 1) % 3]);
-      int x0 = (v0.x + 1.) * width / 2.;
-      int y0 = (v0.y + 1.) * height / 2.;
-      int x1 = (v1.x + 1.) * width / 2.;
-      int y1 = (v1.y + 1.) * height / 2.;
-      line(x0, y0, x1, y1, image, white);
-    }
-  }
+
+  Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+  Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+  Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+  triangle(t0[0], t0[1], t0[2], image, red);
+  triangle(t1[0], t1[1], t1[2], image, white);
+  triangle(t2[0], t2[1], t2[2], image, green);
 
   image.flip_vertically();
   image.write_tga_file("output.tga");
-  delete model;
 }

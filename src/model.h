3d49c46320835cd65b0059fa1789ffad7b36233c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-11-02 19:55:32 +0800

    Perspective projection

--- src/model.h
+++ src/model.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include "geometry.h"
+#include "tgaimage.h"
 #include <vector>
 
 class Model {
 private:
   std::vector<Vec3f> verts_;
-  std::vector<std::vector<int>> faces_;
+  std::vector<std::vector<Vec3i>>
+      faces_; // attention, this Vec3i means vertex/uv/normal
+  std::vector<Vec3f> norms_;
+  std::vector<Vec2f> uv_;
+  TGAImage diffusemap_;
+  void load_texture(std::string filename, const char *suffix, TGAImage &img);
 
 public:
   Model(const char *filename);
@@ -14,5 +20,7 @@ public:
   int nverts();
   int nfaces();
   Vec3f vert(int i);
+  Vec2i uv(int iface, int nvert);
+  TGAColor diffuse(Vec2i uv);
   std::vector<int> face(int idx);
 };

a000b4ea64c16e44f5d0cb35b1f7e2f1e5f85791
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-11-02 19:33:31 +0800

    add debug variable

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,9 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c11", "c++17")
-set_warnings("all", "error")
 
 target("renderer")
     set_kind("binary")
     add_files("src/*.cpp")
+    if is_mode("debug") then
+        add_defines("DEBUG")
+    end
